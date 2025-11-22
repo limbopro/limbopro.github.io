55673cf7601fe418eb00d2329fe910350cb55afa
limbopro <service.limbopro.xyz@gmail.com>
2025-11-22 11:26:21 +0800

    assets && html [2025-11-22 11:26:21]

--- tools/jwksm/script.js
+++ tools/jwksm/script.js
@@ -51,7 +51,8 @@ async function fetchCodes() {
         "2022_best_netflav.json",
         "2021_best_netflav.json",
         "2020_best_netflav.json",
-        "2wFav.json"
+        "2wFav.json",
+        "3wFav.json"
     ];
 
     window.dataBestrated = await loadJsonFiles(iBestratedfiles, "./bestrated/");
